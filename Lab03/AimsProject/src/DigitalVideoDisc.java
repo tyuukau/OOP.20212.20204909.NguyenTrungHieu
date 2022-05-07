76fd845e5b04a0f1cabd5080966e4a33721b1a95
tyuukau <bachocnhi2002@gmail.com>
2022-05-07 12:05:14 +0700

    Added constructors method in DigitalVideoDisc class

--- Lab03/AimsProject/src/DigitalVideoDisc.java
+++ Lab03/AimsProject/src/DigitalVideoDisc.java
@@ -41,4 +41,32 @@ public class DigitalVideoDisc {
         return this.cost;
     }
 
+    public DigitalVideoDisc(String title) {
+        this.title = title;
+    }
+
+
+    public DigitalVideoDisc(String title, String category, float cost) {
+        this.title = title;
+        this.category = category;
+        this.cost = cost;
+    }
+
+
+    public DigitalVideoDisc(String title, String category, String director, float cost) {
+        this.title = title;
+        this.category = category;
+        this.director = director;
+        this.cost = cost;
+    }
+    
+
+    public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
+        this.title = title;
+        this.category = category;
+        this.director = director;
+        this.length = length;
+        this.cost = cost;
+    }
+
 }
