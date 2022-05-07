e45eb862cd72d7eee6a5fc05ff320a85261b4e8b
tyuukau <bachocnhi2002@gmail.com>
2022-05-07 16:41:34 +0700

    Initiliased Cart class
    Added MAX_NUMBERS_ORDERED.
    Added itemsOrdered list to store the list of objects currently in the cart.

---
+++ Lab03/AimsProject/src/Cart.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class Cart {
+
+    public static final int MAX_NUMBERS_ORDERED = 20;
+
+    private ArrayList<DigitalVideoDisc> itemsOrdered = new ArrayList<DigitalVideoDisc>();
+
+}
