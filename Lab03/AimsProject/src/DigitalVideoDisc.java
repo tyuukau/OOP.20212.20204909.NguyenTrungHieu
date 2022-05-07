ffa2bd2dce41f8f080280bc17ba19c436e567f83
tyuukau <bachocnhi2002@gmail.com>
2022-05-07 11:41:06 +0700

    Added accessors and mutators for DigitalVideoDisc class

--- Lab03/AimsProject/src/DigitalVideoDisc.java
+++ Lab03/AimsProject/src/DigitalVideoDisc.java
@@ -6,4 +6,39 @@ public class DigitalVideoDisc {
     private int length;
     private float cost;
 
+    /**
+     * @return String return the title
+     */
+    public String getTitle() {
+        return this.title;
+    }
+
+    /**
+     * @return String return the category
+     */
+    public String getCategory() {
+        return this.category;
+    }
+
+    /**
+     * @return String return the director
+     */
+    public String getDirector() {
+        return this.director;
+    }
+
+    /**
+     * @return int return the length
+     */
+    public int getLength() {
+        return this.length;
+    }
+
+    /**
+     * @return float return the cost
+     */
+    public float getCost() {
+        return this.cost;
+    }
+
 }
