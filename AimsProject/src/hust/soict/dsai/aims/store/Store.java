1e5486e8197530f9d356be4828064d82d4815d8b
tyuukau <bachocnhi2002@gmail.com>
2022-05-23 16:59:08 +0700

    Added Store, StoreText classes

---
+++ AimsProject/src/hust/soict/dsai/aims/store/Store.java
@@ -0,0 +1,41 @@
+package hust.soict.dsai.aims.store;
+
+import hust.soict.dsai.aims.disc.DigitalVideoDisc;
+import java.util.ArrayList;
+
+public class Store {
+    private ArrayList<DigitalVideoDisc> itemsInStore = new ArrayList<DigitalVideoDisc>();
+
+	public void addDVD(DigitalVideoDisc disc) {
+		if (this.itemsInStore.contains(disc)) {
+			System.out.println(disc.getTitle() + " is already in the store.");
+		} else {
+			this.itemsInStore.add(disc);
+			System.out.println(disc.getTitle() + " has been added to the store.");
+		}
+	}
+	
+	public void removeDVD(DigitalVideoDisc disc) {
+		if (this.itemsInStore.remove(disc)) {
+			System.out.println(disc.getTitle() + " has been removed from the store.");
+		} else {
+			System.out.println(disc.getTitle() + " was not found in the store.");;
+		}
+	}
+
+	public void print() {
+		System.out.println("\n");
+		System.out.println("*************AVAILABLE MEDIA IN STORE**************");
+		for (int i = 0; i < itemsInStore.size(); i++) {
+			System.out.println(Integer.toString(i+1) + "." + "\t" + this.itemsInStore.get(i).toString());
+		}
+		System.out.println("***************************************************");
+		System.out.println("\n");
+	}
+	
+	public ArrayList<DigitalVideoDisc> getItemsInStore() {
+		return this.itemsInStore;
+	}
+
+
+}
---
+++ AimsProject/src/hust/soict/dsai/test/store/StoreTest.java
@@ -0,0 +1,30 @@
+package hust.soict.dsai.test.store;
+
+import hust.soict.dsai.aims.disc.DigitalVideoDisc;
+import hust.soict.dsai.aims.store.Store;
+
+public class StoreTest {
+
+	public static void main(String[] args) {
+		//Create the store
+		Store myStore = new Store();
+
+		//Create DVDs
+		DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
+		DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
+		DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
+
+		//Try adding DVDs
+		myStore.addDVD(dvd2);
+		myStore.addDVD(dvd1);
+		myStore.addDVD(dvd3);
+		myStore.addDVD(dvd1);
+		myStore.addDVD(dvd1);
+		myStore.print();
+
+		//Try removing DVDs
+		myStore.removeDVD(dvd3);
+		myStore.removeDVD(dvd1);
+		myStore.print();
+	}
+}
