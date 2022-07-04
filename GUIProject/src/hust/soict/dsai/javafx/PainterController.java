236f6537d7a6f79ccde4153265607b9f851abeb4
tyuukau <bachocnhi2002@gmail.com>
2022-07-04 12:46:09 +0700

    Updated Painter
    Lab 8 : -> | Part 4

--- GUIProject/src/hust/soict/dsai/javafx/PainterController.java
+++ GUIProject/src/hust/soict/dsai/javafx/PainterController.java
@@ -2,12 +2,18 @@ package hust.soict.dsai.javafx;
 
 import javafx.event.*;
 import javafx.fxml.FXML;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 
 public class PainterController {
+
+    private boolean isPen = true;
+
+    @FXML
+    private ToggleGroup toggleGroup1;
 
     @FXML
     private Pane drawingAreaPane;
@@ -18,9 +24,22 @@ public class PainterController {
     }
 
     @FXML
+    void penButtonPressed(ActionEvent event) {
+        isPen = true;
+    }
+
+    @FXML
+    void eraserButtonPressed(ActionEvent event) {
+        isPen = false;
+    }
+
+    @FXML
     void drawingAreaMouseDragged(MouseEvent event) {
-        Circle newCircle = new Circle(event.getX(), event.getY(), 4, Color.BLACK);
-        drawingAreaPane.getChildren().add(newCircle);
+        if (isPen) {
+            drawingAreaPane.getChildren().add(new Circle(event.getX(), event.getY(), 4, Color.BLACK));
+        } else {
+            drawingAreaPane.getChildren().add(new Circle(event.getX(), event.getY(), 4, Color.WHITE));
+        }
     }
 
 }
